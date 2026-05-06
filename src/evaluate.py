f7bce8d57b4e3e0757ea0b68a398baea11181ad0
ssrhaso <hasaana2005@gmail.com>
2026-05-06 14:46:11 +0100

    CHORE: Add copyright header to evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Hasaan Ahmad et al. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for full text.
+
 """Evaluation and visualisation utilities for wildfire classification models.
 
 Loads trained checkpoints, runs inference on the test set, and produces:
