bcbb6addf04e3204b39bae190644dce970cb4bd2
ssrhaso <hasaana2005@gmail.com>
2026-05-07 10:18:41 +0100

    CHORE: Add copyright header to analyse_results.py

--- src/analyse_results.py
+++ src/analyse_results.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 Hasaan Ahmad et al. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for full text.
+
 """Statistical analysis of freezing ablation results.
 
 Reads JSON results and produces summary tables, statistical tests,
